f006e11d2ee9829e92354c181cf25fcd273e88be
Robert Hails <rhails777@gmail.com>
2024-02-02 13:40:01 -0500

    homepage styling and metadata url update

--- src/components/navigation/Navlink.jsx
+++ src/components/navigation/Navlink.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 
-export default function NavLink({ name, href }) {
+export default function NavLink({ name, href, onClick }) {
   return (
     <Link
       href={href}
       className="text-lg py-1 hover:text-primary hover:border-b-2 hover:border-primary"
+      onClick={onClick}
     >
       <span>{name}</span>
     </Link>
